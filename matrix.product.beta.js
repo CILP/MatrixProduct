601267900b2cd685d9d3a3321bb10c87647e4c2a
Carlos Linares <cilp2912@gmail.com>
2016-11-25 23:21:47 -0600

    Explanation added and function exposition with module pattern

--- matrix.product.beta.js
+++ matrix.product.beta.js
@@ -1,80 +1,129 @@
-function getSize(m){
+/*
+  The multiply function (a.k.a product) receive two arrays
+  that could be as the show above:
 
-  return [m.length, m[0].length];
-}
+    For a row vector:
+      [
+        [a, b, c]
+      ]
+    
+    For a column vector:
+      [
+        [x],
+        [y],
+        [z]
+      ]
 
-function isColumn(m){
+    For rectangle row vector:
+      [
+        [a, b, c],
+        [d, e, f]
+      ]
 
-  return m[0].length < m.length ? true : false;
-}
+    For rectangle column vector:
+      [
+        [x, i],
+        [y, j],
+        [z, k]
+      ]
 
-function canBeMultiply(a, b){
+  The result will be the Tensor product or a Dot product
+  depending on the type of arrays passed as arguments.
 
-  return a[1] === b[0] ? true : false;
-}
+  For example:
 
-function buildProduct(rn, cn, m){
+    Dot Product:
+      [
+        [a]
+      ]
 
-  var result = [],
-      tmp,
-      i, j;
+    Tensor product: 
+      [
+        [a, b, c],
+        [d, e, f],
+        [g, h, i]
+      ] 
+*/
 
-  for (i = 0; i !== rn; i++){
+(function(){
 
-    tmp = [];
-    for (j = 0; j !== cn; j++){
+  function getSize(m){
 
-      (function(i, j, m){
-
-        tmp.push(function(a, b){
-
-          var products = [],
-              k;
-
-          for (k = 0; k !== m; k++){
-            products.push(
-              a[i][k] * b[k][j]
-            );
-          }
-
-          return products.reduce(function(x, y){
-            return x + y;
-          });
-        });
-      })(i, j, m);
-    }
-
-    result.push(tmp);
+    return [m.length, m[0].length];
   }
 
-  return result;
-}
+  function canBeMultiply(a, b){
 
-function multiply(a, b){
+    return a[1] === b[0] ? true : false;
+  }
 
-  var aSize = getSize(a),
-      bSize = getSize(b),
-      products = [],
-      result = [],
+  function buildProduct(rn, cn, m){
+
+    var result = [],
+        tmp,
+        i, j;
+
+    for (i = 0; i !== rn; i++){
+
       tmp = [];
-  
-  if (canBeMultiply(aSize, bSize)){
+      for (j = 0; j !== cn; j++){
 
-    products = buildProduct(aSize[0], bSize[1], aSize[1]);
+        (function(i, j, m){
 
-    products.forEach(function(p){
-    
-      tmp = [];
+          tmp.push(function(a, b){
 
-      p.forEach(function(fn){
-        tmp.push(fn(a, b));
-      });
+            var products = [],
+                k;
+
+            for (k = 0; k !== m; k++){
+              products.push(
+                a[i][k] * b[k][j]
+              );
+            }
+
+            return products.reduce(function(x, y){
+              return x + y;
+            });
+          });
+        })(i, j, m);
+      }
 
       result.push(tmp);
-    });
-  } else {
-    throw new Error('multiply(): Cannot multiply a * b');
+    }
+
+    return result;
   }
-  
-  return result;
-}
+
+  function multiply(a, b){
+
+    var aSize = getSize(a),
+        bSize = getSize(b),
+        result = [],
+        products, tmp;
+    
+    if (canBeMultiply(aSize, bSize)){
+
+      products = buildProduct(aSize[0], bSize[1], aSize[1]);
+
+      products.forEach(function(p){
+      
+        tmp = [];
+
+        p.forEach(function(fn){
+          tmp.push(fn(a, b));
+        });
+
+        result.push(tmp);
+      });
+    } else {
+      throw new Error('multiply(): Cannot multiply a * b');
+    }
+    
+    return result;
+  }
+
+  return {
+    product: multiply
+  };
+
+})();
